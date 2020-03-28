5c1be60135cd8cc814aa80451a7d8a85cbecf3bc
axing <axing0905@gmail.com>
2020-03-28 16:32:45 +0800

    前端项目引入 iview 布局完成

--- generator-ui-vue/README.md
+++ generator-ui-vue/README.md
@@ -8,6 +8,11 @@
 - cnpm run build 把前端页面发布的 spring boot 项目中
     - 把 dist 目录 html文件放入 generator/src/main/resources/templates中
     - 把css/img/js 目录放在     generator/src/main/resources/static 中
+    
+## 项目依赖和版本 
+- 具体查看 package.json dependencies{...}
+- vue-router 路由
+- iview ui 组件
 
 ## Project setup
 ```
--- generator-ui-vue/src/main.js
+++ generator-ui-vue/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import Routers from './router.js';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+//在一个模块化工程中使用它，必须要通过 Vue.use() 明确地安装路由功能
+Vue.use(VueRouter);
+Vue.use(iView);
 
 Vue.config.productionTip = false
+// The routing configuration
+const RouterConfig = {
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
