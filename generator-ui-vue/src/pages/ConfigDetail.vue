6ba434c2c6cd7a23f4edd2747cb5f10a69f81e7e
axing <axing0905@gmail.com>
2020-03-28 20:20:18 +0800

    数据库连接表单页面开发

--- generator-ui-vue/src/pages/ConfigDetail.vue
+++ generator-ui-vue/src/pages/ConfigDetail.vue
@@ -1,34 +1,39 @@
 <template>
     <div>
-        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
-            <FormItem label="Name" prop="name">
-                <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+        <Form ref="formValidate" :model="formValidate" :label-width="100">
+            <FormItem label="表名">
+                <Input v-model="formValidate.name" placeholder="person"></Input>
             </FormItem>
-            <FormItem label="E-mail" prop="mail">
-                <Input v-model="formValidate.mail" placeholder="Enter your e-mail"></Input>
+            <FormItem label="Java实体类名">
+                <Input v-model="formValidate.mail" placeholder="Person"></Input>
             </FormItem>
-            <FormItem label="City" prop="city">
-                <Select v-model="formValidate.city" placeholder="Select your city">
-                    <Option value="beijing">New York</Option>
-                    <Option value="shanghai">London</Option>
-                    <Option value="shenzhen">Sydney</Option>
-                </Select>
+            <FormItem label="主键(选填)">
+                <Input v-model="formValidate.city" placeholder="primary key, such as id"></Input>
+
             </FormItem>
-            <FormItem label="Date">
+            <FormItem label="项目所在目录">
+                <Input v-model="formValidate.name" placeholder="D:\workspace\example"></Input>
+            </FormItem>
+            <FormItem label="项目基础包名">
+                <Input v-model="formValidate.mail" placeholder="com.company.project"></Input>
+            </FormItem>
+            <FormItem label="ftl模板所在目录">
+                <Input v-model="formValidate.city" placeholder="D:\workspace\example"></Input>
+
+            </FormItem>
                 <Row>
                     <Col span="11">
-                        <FormItem prop="date">
-                            <DatePicker type="date" placeholder="Select date" v-model="formValidate.date"></DatePicker>
+                        <FormItem label="controller包名">
+                            <Input v-model="formValidate.city" placeholder="controller"></Input>
                         </FormItem>
                     </Col>
-                    <Col span="2" style="text-align: center">-</Col>
+                    <!-- <Col span="2"></Col> -->
                     <Col span="11">
-                        <FormItem prop="time">
-                            <TimePicker type="time" placeholder="Select time" v-model="formValidate.time"></TimePicker>
+                        <FormItem label="service包名">
+                            <Input v-model="formValidate.city" placeholder="service"></Input>
                         </FormItem>
                     </Col>
                 </Row>
-            </FormItem>
             <FormItem label="Gender" prop="gender">
                 <RadioGroup v-model="formValidate.gender">
                     <Radio label="male">Male</Radio>
@@ -66,50 +71,15 @@
                     date: '',
                     time: '',
                     desc: ''
-                },
-                ruleValidate: {
-                    name: [
-                        { required: true, message: 'The name cannot be empty', trigger: 'blur' }
-                    ],
-                    mail: [
-                        { required: true, message: 'Mailbox cannot be empty', trigger: 'blur' },
-                        { type: 'email', message: 'Incorrect email format', trigger: 'blur' }
-                    ],
-                    city: [
-                        { required: true, message: 'Please select the city', trigger: 'change' }
-                    ],
-                    gender: [
-                        { required: true, message: 'Please select gender', trigger: 'change' }
-                    ],
-                    interest: [
-                        { required: true, type: 'array', min: 1, message: 'Choose at least one hobby', trigger: 'change' },
-                        { type: 'array', max: 2, message: 'Choose two hobbies at best', trigger: 'change' }
-                    ],
-                    date: [
-                        { required: true, type: 'date', message: 'Please select the date', trigger: 'change' }
-                    ],
-                    time: [
-                        { required: true, type: 'string', message: 'Please select time', trigger: 'change' }
-                    ],
-                    desc: [
-                        { required: true, message: 'Please enter a personal introduction', trigger: 'blur' },
-                        { type: 'string', min: 20, message: 'Introduce no less than 20 words', trigger: 'blur' }
-                    ]
                 }
             }
         },
         methods: {
             handleSubmit (name) {
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('Success!');
-                    } else {
-                        this.$Message.error('Fail!');
-                    }
-                })
+                this.$Message.success('Success!');
             },
             handleReset (name) {
-                this.$refs[name].resetFields();
+                //this.$refs[name].resetFields();
             }
         }
     }
