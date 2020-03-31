697741ff215099acb42e347770eca4d0530c66ea
axing <axing0905@gmail.com>
2020-03-31 16:22:20 +0800

    添加 xml 方式生成代码方式,官方文档学习  xml 配置详解

---
+++ src/main/java/cn/javabus/generator/test/BaseEntity.java
@@ -0,0 +1,7 @@
+package cn.javabus.generator.test;
+
+/**
+ * @author ou.zhenxing on 2020-03-31.
+ */
+public class BaseEntity {
+}
---
+++ src/test/java/cn/javabus/generator/mgb/RunGenerator.java
@@ -0,0 +1,48 @@
+package cn.javabus.generator.mgb;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ou.zhenxing on 2020-03-31.
+ * <p>
+ * 文档  http://mybatis.org/generator/running/runningWithJava.html
+ */
+public class RunGenerator {
+    public static void main(String[] args) {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+
+        try {
+            Resource xmlConfigResource = new ClassPathResource("MybatisGeneratorConfig.xml");
+            File configFile = xmlConfigResource.getFile();
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
